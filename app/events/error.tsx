a5ce0fad3180f5253f5b6eeecc4e744b98b251c9
Petro Krevenets <peter.k.nets@gmail.com>
2026-02-02 12:09:01 +0200

    refactor: rename Error component to EventError and enhance error display for development mode

--- app/events/error.tsx
+++ app/events/error.tsx
@@ -1,9 +1,14 @@
 "use client";
 
-export default function Error({ error }: { error: Error }) {
+export default function EventError({ error }: { error: Error }) {
+  const isDev = process.env.NODE_ENV === "development";
+
   return (
     <div className="text-destructive">
-      <pre className="text-sm text-wrap">{error.stack}</pre>
+      <p>Something went wrong.</p>
+      {isDev && error.stack && (
+        <pre className="text-sm text-wrap">{error.stack}</pre>
+      )}
     </div>
   );
 }
